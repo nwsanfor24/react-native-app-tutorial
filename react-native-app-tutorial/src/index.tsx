82dd94dcea7ee1ba1507703bafe514326fff85c1
Nathan Sanford <nwsanfor24@gmail.com>
2020-08-17 12:54:12 -0400

    imported App Registry from react-native

--- react-native-app-tutorial/src/index.tsx
+++ react-native-app-tutorial/src/index.tsx
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { AppRegistry } from 'react-native';
+
+AppRegistry.registerComponent('App', () => App);
+
+AppRegistry.runApplication('App', {
+  initialProps: {},
+  rootTag: document.getElementById('react-app')
+});
 
 ReactDOM.render(
   <React.StrictMode>
